850e8f14c8ab2ed3bbc421ff120ae46b75ad0755
muhlenbrock <racm@live.cl>
2016-12-06 19:13:54 -0300

    img footer

--- include/footer.php
+++ include/footer.php
@@ -2,20 +2,17 @@
 <footer class="logosydatos">
   <div class="container">
     <div class="row">
-      <div class="col-lg-5">
-        <img src="img/logoweb.png" alt="" class="img-responsive">
+      <div class="col-lg-3">
+        <img src="images/logo1.png" alt="" class="img-responsive">
       </div>
       <div class="col-lg-2">
-        <a href="http://www.userena.cl/index.php/acreditacion"><img class="center-block img-responsive" src="img/logocnasolo.png"/></a>
+        <a href="#"><img class="center-block img-responsive" src="images/logo2.png"/></a>
       </div>
-      <div class="col-lg-5">
-        <address>
-          <strong>Escuela de Periodismo Universidad de La Serena</strong><br>
-          Campus Andrés Bello, Avenida Raúl Bitrán Nachary 1305<br>
-          La Serena, Chile<br>
-          <abbr title="Télefono"></abbr> +56 (51) 2204352<br>
-          <a href="mailto:#">eperiodismo@userena.cl</a>
-        </address>
+      <div class="col-lg-3">
+        <a href="#"><img class="center-block img-responsive" src="images/logo3.png"/></a>
+      </div>
+      <div class="col-lg-4">
+        <a href="#"><img class="center-block img-responsive" src="images/logo4.png"/></a>
       </div>
     </div>
   </div>
