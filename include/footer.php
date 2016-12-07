c21afe8236cb655033051304d983d2e3fa7b4298
muhlenbrock <racm@live.cl>
2016-12-07 13:12:40 -0300

    observatorio-laboral-v.1.1

--- include/footer.php
+++ include/footer.php
@@ -1,9 +1,17 @@
 <!-- Footer -->
 <footer class="">
   <div class="container">
+<div class="row">
+  <div class="col-md-12" >
+    <div class="col-md-5 col-xs-5" style="height: 5px;background-color: #E73634;">
+    </div>
+    <div class="col-md-7 col-xs-7" style="height: 5px;background-color: #0b7bd0;">
+    </div>
+  </div>
+</div>
     <div class="row">
       <div class="col-md-12">
-        <div class="hilonoticias" style="height:200px;">
+        <div class="footer-observatorio-ucn">
           <div class="col-lg-3" style="    min-height: 100%;display: flex;align-items: center;">
             <img src="images/logo1.png" alt="" class="img-responsive">
           </div>
