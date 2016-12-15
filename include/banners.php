5de2b1b4a2218344ced9106bd00fa99869367250
Mauricio <mauricio.leonheart@gmail.com>
2016-12-15 13:53:29 -0300

    Agregado un banner más

--- include/banners.php
+++ include/banners.php
@@ -1,3 +1,6 @@
 <div class="banner-image">
-  <img class="banner-image-img img-responsive" src="http://placehold.it/360x100"
+  <img class="banner-image-img img-responsive" src="http://placehold.it/360x100" /img>
+</div>
+<div class="banner-image">
+  <img class="banner-image-img img-responsive" src="http://placehold.it/360x100" /img>
 </div>
