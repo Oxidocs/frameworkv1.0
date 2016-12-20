cfe6bf092b4e8bda3e688beaf1f8b8debf91d98a
muhlenbrock <racm@live.cl>
2016-12-20 18:32:22 -0300

    index v1.0

--- include/banners.php
+++ include/banners.php
@@ -1,6 +1,6 @@
 <div class="banner-image">
-  <img class="banner-image-img img-responsive" src="http://placehold.it/360x100" /img>
+  <img class="banner-image-img img-responsive center-block" src="http://placehold.it/360x100" /img>
 </div>
 <div class="banner-image">
-  <img class="banner-image-img img-responsive" src="http://placehold.it/360x100" /img>
+  <img class="banner-image-img img-responsive center-block" src="http://placehold.it/360x100" /img>
 </div>
--- include/footer.php
+++ include/footer.php
@@ -1,32 +1,32 @@
 <!-- Footer -->
 <footer class="">
   <div class="container">
-<div class="row">
-  <div class="col-md-12" >
-    <div class="col-md-5 col-xs-5" style="height: 5px;background-color: #E73634;">
-    </div>
-    <div class="col-md-7 col-xs-7" style="height: 5px;background-color: #0b7bd0;">
-    </div>
-  </div>
-</div>
     <div class="row">
-      <div class="col-md-12">
-        <div class="footer-observatorio-ucn">
-          <div class="col-lg-3" style="    min-height: 100%;display: flex;align-items: center;">
-            <img src="images/footer/logo1.png" alt="" class="img-responsive">
-          </div>
-          <div class="col-lg-2" style="    min-height: 100%;display: flex;align-items: center;">
-            <a href="#"><img class="center-block img-responsive" src="images/footer/logo2.png"/></a>
-          </div>
-          <div class="col-lg-3" style="    min-height: 100%;display: flex;align-items: center;">
-            <a href="#"><img class="center-block img-responsive" src="images/footer/logo3.png"/></a>
-          </div>
-          <div class="col-lg-4" style="    min-height: 100%;display: flex;align-items: center;">
-            <a href="#"><img class="center-block img-responsive" src="images/footer/logo4.png"/></a>
-          </div>
+      <div class="col-md-12" >
+        <div class="col-md-5 col-xs-5" style="height: 5px;background-color: #E73634;">
+        </div>
+        <div class="col-md-7 col-xs-7" style="height: 5px;background-color: #0b7bd0;">
         </div>
       </div>
     </div>
+  <div class="row">
+    <div class="col-md-12">
+      <div class="footer-observatorio-ucn">
+        <div class="col-lg-3 div-logo">
+          <a href="#"><img class="center-block img-responsive div-logo-img" src="images/footer/logo1.png" alt=""></a>
+        </div>
+        <div class="col-lg-2 div-logo">
+          <a href="#"><img class="center-block img-responsive div-logo-img" src="images/footer/logo2.png"/></a>
+        </div>
+        <div class="col-lg-3 div-logo">
+          <a href="#"><img class="center-block img-responsive div-logo-img" src="images/footer/logo3.png"/></a>
+        </div>
+        <div class="col-lg-4 div-logo">
+          <a href="#"><img class="center-block img-responsive div-logo-img" src="images/footer/logo4.png"/></a>
+        </div>
+      </div>
+    </div>
+  </div>
   </div>
 </footer>
 
